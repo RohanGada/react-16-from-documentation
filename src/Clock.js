df05c0a2212e097d5126713ee9331c84644588f8
Rohan Gada <rohangadaconsultant@localhost>
2017-10-05 17:00:46 +0530

    Handling Event and eventPropagation

--- src/Clock.js
+++ src/Clock.js
@@ -1,35 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import './App.css';
 
 class Clock extends Component {
-  tick(){
-    this.setState((prevState,props) => {
-      return ({
-      date:new Date(prevState.date.setSeconds(prevState.date.getSeconds()+1))
-    })})
-  }
+
   constructor(props) {
     super(props); //baseconstructor call
     console.log(props)
-    this.state = {date: props.date};
+    this.state = {
+      date: props.date,
+      isToggleOn: true
+    };
   }
-  componentDidMount(){
+  tick() {
+    this.setState((prevState, props) => {
+      // console.log(prevState)
+      return ({
+        date: new Date(prevState.date.setSeconds(prevState.date.getSeconds() + 1))
+      })
+    })
+  }
+  componentDidMount() {
     console.log('Mounted')
-    this.timerId =setInterval(
-      ()=>{this.tick();
-      console.log('HEre')}
-    ,1000
-  );
+    this.timerId = setInterval(() => {
+      this.tick();
+    }, 1000);
   }
-  componentWillUnmount(){
+
+  activateLasers() {
+    console.log('clicked')
+    this.setState((prevState) => {
+      return ({
+        isToggleOn: !prevState.isToggleOn
+      })
+    })
+  }
+
+  componentWillUnmount() {
     console.log('Unmounted')
     clearInterval(this.timerId);
+  }
+
+  handleClick(e) {
+    console.log(e)
+    e.preventDefault()
   }
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this
+            .state
+            .date
+            .toLocaleTimeString()}.</h2>
+        <button onClick={this.activateLasers.bind(this)}>
+          {this.state.isToggleOn
+            ? "YES"
+            : "NO"}
+        </button>
+        <a href='' onClick={this.handleClick}>
+          Click me
+        </a>
       </div>
     );
   }
