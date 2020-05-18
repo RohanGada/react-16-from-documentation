cbd6605caef136000ebc40ccd1bd7e33aab1e7c6
RohanGada <gadarohan17@gmail.com>
2020-05-18 14:57:48 +0530

    reset to latest react and redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
-// import Clock from './Clock';
-// import Calculator from './Calculator';
-import SignUpDialog from './SignUpDialog';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* <Clock date={new Date(new Date().setMinutes(20))}/> */}
-        {/* <Clock warn={true} date={new Date(new Date().setMinutes(20))}/> */}
-        {/* <Calculator/> */}
-      <SignUpDialog/>
-        {/* < form >
-          <label>
-            Name:
-            <input type="text" value={this.state.name} name="name" onChange={this.handleClick}/>
-          </label>
-          < input type="submit" value="Submit"/>
-        </form> */}
-      </div>
-    );
-  }
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
 }
 
 export default App;
